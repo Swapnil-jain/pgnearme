93600b349cca2273b35fb92b41dbbc7c79c16d5b
Swapnil Jain <jainswapnil138@gmail.com>
2025-04-08 20:27:48 +0530

    Add Google Analytics tracking scripts to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import Script from 'next/script'
 
 export const metadata: Metadata = {
   title: {
@@ -35,7 +36,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>{children}
+      <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=G-YYM8HD7RGW`}
+            strategy="lazyOnload"
+            async
+            defer
+        />
+        <Script id="google-analytics" strategy="lazyOnload">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-YYM8HD7RGW', {
+              page_path: window.location.pathname,
+              transport_type: 'beacon'
+            });
+          `}
+        </Script>
+      </body>
     </html>
   )
 }
